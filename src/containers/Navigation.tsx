26f5d6cc8524b2e476e5f0869a69cb7160c9d685
Benjamin <benabolade@gmail.com>
2025-06-16 02:34:27 +0100

    (chore): try fix for unexisting deployment module

--- src/containers/Navigation.tsx
+++ src/containers/Navigation.tsx
@@ -1,9 +1,10 @@
 import { forwardRef, useState } from "react";
 import { IconPencil } from "@tabler/icons-react";
-import LayoutControls from "../components/navigation/layout-controls";
+
 import Settings from "../components/Settings";
 import Divider from "../components/common/divider";
 import Refresh from "../components/Refresh";
+import LayoutControls from "../components/layout-controls";
 
 export default function Navigation() {
    const [projectName, setProjectName] = useState("Untitled...");
@@ -15,9 +16,9 @@ export default function Navigation() {
             <div className="px-2 *:stroke-gray-800 dark:*:stroke-gray-100">
             <svg width="40" height="40" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
                {/* <rect x="10" y="10" width="180" height="180" rx="30" fill="#888888"/> */}
-               <path d="M60 70L40 90L60 110" stroke="#555555" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
-               <path d="M140 70L160 90L140 110" stroke="#555555" stroke-width="8" stroke-linecap="round" stroke-linejoin="round"/>
-               <path d="M85 130L115 50" stroke="gray" stroke-width="15" stroke-linecap="round"/>
+               <path d="M60 70L40 90L60 110" stroke="#555555" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+               <path d="M140 70L160 90L140 110" stroke="#555555" strokeWidth="8" strokeLinecap="round" strokeLinejoin="round"/>
+               <path d="M85 130L115 50" stroke="gray" strokeWidth="15" strokeLinecap="round"/>
             </svg>
 
             </div>
